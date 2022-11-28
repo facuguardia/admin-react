af366cfef99a9b6723326d1fa2ffbb8911c76f73
Facundo Guardia <contacto@facuguardia.com>
2022-11-27 21:19:00 -0300

    Second commit (register)

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,34 +1,18 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 //Icons
 import { FaRegEyeSlash, FaRegEye } from "react-icons/fa";
 
 const Login = () => {
-  return (
-    <div className="bg-secondary-100 flex flex-col items-center justify-center gap-4 p-8 rounded-xl">
-      <h1 className="text-3xl uppercase font bold tracking-[5px] text-white font-medium">
-        Log In
-      </h1>
-      <form action="" className="flex flex-col gap-3">
-        <div className="relative flex items-center gap-2">
-          <input
-            type="text"
-            placeholder="Email"
-            className="p-2 rounded-xl text-black"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="p-2 rounded-xl text-black"
-          />{" "}
-          <FaRegEye className="absolute top-1/2 -translate-y-1/2 right-2 text-black" />
-        </div>
+  const [showPassword, setShowPassword] = useState(false);
 
-        <button
-          type="submit"
-          className="flex items-center justify-center gap-4 bg-blue-500 py-1 rounded-xl font-medium"
-        >
-          Login
-        </button>
+  return (
+    <>
+      <div className="bg-secondary-100 flex flex-col items-center justify-center gap-4 p-8 rounded-xl">
+        <h1 className="text-3xl uppercase font bold tracking-[5px] text-white font-medium">
+          Log In
+        </h1>
         <button
           type="submit"
           className="flex items-center justify-center gap-4 p-3 rounded-xl"
@@ -41,8 +25,48 @@ const Login = () => {
           />
           Login with google
         </button>
-      </form>
-    </div>
+        <form action="" className="flex flex-col gap-3">
+          <div className="relative flex items-center gap-2">
+            <input
+              type="email"
+              placeholder="Email"
+              className="p-2 rounded-xl text-black"
+            />
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              maxLength={20}
+              className="p-2 rounded-xl text-black"
+            />{" "}
+            {showPassword ? (
+              <FaRegEyeSlash
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute top-1/2 -translate-y-1/2 right-3 text-black hover:cursor-pointer"
+              />
+            ) : (
+              <FaRegEye
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute top-1/2 -translate-y-1/2 right-3 text-black hover:cursor-pointer"
+              />
+            )}
+          </div>
+
+          <button
+            type="submit"
+            className="flex items-center justify-center gap-4 bg-primary hover:bg-primary/80 py-1 rounded-xl font-medium text-black hover:text-white"
+          >
+            Login
+          </button>
+        </form>
+        <div>
+          <Link>Recover password ?</Link>
+        </div>
+        <div className="flex items-center gap-2">
+          <p>You do not have an account ?</p>
+          <Link to="/auth/register" className="text-primary hover:text-primary/80">Register</Link>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <>
+      <div className="bg-secondary-100 flex flex-col items-center justify-center gap-4 p-8 rounded-xl">
+        <h1 className="text-3xl uppercase font bold tracking-[5px] text-white font-medium">
+          Create an <b className="text-primary">account</b>
+        </h1>
+        <button
+          type="submit"
+          className="flex items-center justify-center gap-4 p-2"
+        >
+          {" "}
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2991/2991148.png"
+            alt="google.com"
+            className="h-4 w-4"
+          />
+          Register with Google
+        </button>
+        <form action="" className="grid items-center gap-3">
+          <div className="relative grid gap-4">
+            <input
+              type="text"
+              placeholder="User Name"
+              className="p-2 rounded-xl text-black"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="p-2 rounded-xl text-black"
+            />
+            <input
+              placeholder="Password"
+              maxLength={20}
+              className="p-2 rounded-xl text-black"
+            />{" "}
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              maxLength={20}
+              className="p-2 rounded-xl text-black"
+            />{" "}
+          </div>
+          <button
+            to="/"
+            type="submit"
+            className="flex items-center justify-center gap-4 bg-primary hover:bg-primary/80 py-1 rounded-xl font-medium text-black hover:text-white"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default Register
+export default Register;
